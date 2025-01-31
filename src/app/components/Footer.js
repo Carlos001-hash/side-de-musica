21e297bd0be10caef532df7ff009e4e0a49ed1ca
Carlos Eduardo <81780275+Carlos001-hash@users.noreply.github.com>
2025-01-31 02:44:23 -0300

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,20 +1,31 @@
+"use client"
 
 import Image from "next/image";
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBackwardStep, faPlay, faVolumeHigh, faShuffle, faRepeat, faSliders, faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
+import { faBackwardStep, faPlay, faPause, faVolumeHigh, faShuffle, faRepeat, faSliders, faBarsStaggered } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
+    const [playMusic, setPlayMusic] = useState(false)
     return (
-        <footer className=" absolute bottom-0 mt-auto w-full h-20 bg-gray-950 rounded-none md:rounded-b-xl flex flex-row items-center justify-between border-t md:border-none border-red-500">
+        <footer className=" absolute bottom-0 mt-auto w-full h-20 bg-gray-950 rounded-none md:rounded-b-xl flex flex-row items-center justify-between border-t md:border-none border-gray-800">
             <div className="mx-4 h-full w-full bg-yellow-5000 flex items-center justify-between gap-2">
                 <div className="w-auto md:w-[205px] h-full flex items-center">
-                    <div className=" bg-gray-300 w-9 md:size-12 mr-3 rounded-full overflow-hidden hover:animate-spin">
-                        <Image src="https://lh3.googleusercontent.com/pw/AP1GczPjrv_UWqeDNfCdewXBj8zq0W6i513KwGoVDNNnW2b8kj4RAG5YUfvzmRBpPz9hMEyFyPwWTuxDsWdg6RMIMYNHeENCmCso1M7ZcTp2TjFDU0_d_wf9GuUFiDKPggGkJIYSYAsGvP8LU8NQmeNA81kXrg=w617-h617-s-no-gm?authuser=0" 
-                        width={400} 
-                        height={41.188} 
-                        alt="" />
-                    </div>
+                    {playMusic ? (
+                        <div className=" bg-gray-900 w-9 md:size-12 mr-3 rounded-full overflow-hidden animate-spin">
+                            <Image src="https://lh3.googleusercontent.com/pw/AP1GczPjrv_UWqeDNfCdewXBj8zq0W6i513KwGoVDNNnW2b8kj4RAG5YUfvzmRBpPz9hMEyFyPwWTuxDsWdg6RMIMYNHeENCmCso1M7ZcTp2TjFDU0_d_wf9GuUFiDKPggGkJIYSYAsGvP8LU8NQmeNA81kXrg=w617-h617-s-no-gm?authuser=0" 
+                            width={400} 
+                            height={41.188} 
+                            alt="" />
+                        </div>
+                    ): (
+                        <div className=" bg-gray-900 w-9 md:size-12 mr-3 rounded-full overflow-hidden">
+                            <Image src="https://lh3.googleusercontent.com/pw/AP1GczPjrv_UWqeDNfCdewXBj8zq0W6i513KwGoVDNNnW2b8kj4RAG5YUfvzmRBpPz9hMEyFyPwWTuxDsWdg6RMIMYNHeENCmCso1M7ZcTp2TjFDU0_d_wf9GuUFiDKPggGkJIYSYAsGvP8LU8NQmeNA81kXrg=w617-h617-s-no-gm?authuser=0" 
+                            width={400} 
+                            height={41.188} 
+                            alt="" />
+                        </div>
+                    )}
                     <div className="flex flex-col justify-center">
                         <div className="bg-red-7000 flex items-end no-underline text-base font-normal">
                             <a href="" className="w-12 truncate md:w-36 h-auto font-bold text-white text-sm text-nowrap  hover:underline overflow-hidden">Montagem 101%</a>
@@ -33,9 +44,15 @@ export default function Footer() {
                             <button className="bg-transparent size-6 sm:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
                                 <FontAwesomeIcon icon={faBackwardStep} />
                             </button>
-                            <button className="group/de bg-blue-7000 size-6 sm:size-9 p-0 relative outline-none rounded-full bg-red-600 hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
-                                <FontAwesomeIcon icon={faPlay} />
-                            </button>
+                            {playMusic ? (
+                                <button onClick={() => setPlayMusic(false)} className="group/de bg-blue-7000 size-6 sm:size-9 pr-1 relative outline-none rounded-full bg-red-600 hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
+                                    <FontAwesomeIcon icon={faPause} />
+                                </button>
+                            ):(
+                                <button onClick={() => setPlayMusic(true)} className="group/de bg-blue-7000 size-6 sm:size-9 p-0 relative outline-none rounded-full bg-red-600 hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
+                                    <FontAwesomeIcon icon={faPlay} />
+                                </button>
+                            )}
                             <button className="bg-transparent size-6 sm:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
                                 <FontAwesomeIcon icon={faBackwardStep} className="rotate-180"/>
                             </button>
@@ -51,12 +68,12 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className="inline-flex gap-0">
-                    <button className="bg-transparent size-6 md:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
+                    <button className="bg-transparent size-6 sm:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center text-[15px] sm:text-[22px] text-white">
                         <FontAwesomeIcon icon={faBarsStaggered} />
                     </button>
                     <div className="w-auto h-auto flex flex-row items-center">
-                        <div>
-                            <button className="bg-transparent size-6 md:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center  text-[15px] sm:text-[22px] text-white">
+                        <div className="w-auto h-auto">
+                            <button className="bg-transparent size-6 sm:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center  text-[15px] sm:text-[22px] text-white">
                                 <FontAwesomeIcon icon={faVolumeHigh} />
                             </button>
                         </div>
@@ -66,10 +83,10 @@ export default function Footer() {
                         id="" 
                         min="0"
                         max="100" 
-                        className="w-6 md:w-16 h-1 mx-1 cursor-pointer" 
+                        className="w-6 md:w-16 sm:w-10 h-1 mx-1 cursor-pointer" 
                         />
                     </div>
-                    <button className="bg-transparent size-6 md:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center  text-[15px] sm:text-[22px] text-white">
+                    <button className="bg-transparent size-6 sm:size-9 p-0 relative outline-none rounded-full hover:bg-red-700 hover:drop-shadow-md shadow-yellow-600 flex items-center justify-center  text-[15px] sm:text-[22px] text-white">
                         <FontAwesomeIcon icon={faSliders} />
                     </button>
                 </div>
